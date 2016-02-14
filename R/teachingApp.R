eea95c35c457058266832a86cd386463e541820a
Auburngrads <Auburngrads@live.com>
2016-02-14 15:47:13 -0500

    Updated teachingApp

--- R/teachingApp.R
+++ R/teachingApp.R
@@ -1,11 +1,13 @@
 teachingApp <- function(name, link = FALSE) {
-  
+
+  if(!is.character(name)) stop('name must be a character string')
+
   gitFile <- paste(c(name,'.R'), collapse = '')
   gitPath <- "C:/Users/Jason/OneDrive/Work-Stuff/Computer Systems/GitHub/teaching-apps"
   gitURL  <- 'https://github.com/Auburngrads/teaching-apps/blob/master'
-  
+
   if(!link) dump(name, file = paste(c(gitPath, gitFile), collapse = '/'))
-  
+
   if( link) paste(c(gitURL, gitFile), collapse = '/')
-  
+
 }
