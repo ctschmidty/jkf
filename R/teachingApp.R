a26b519750c8e2b9f2e7b23c665dfa0e14d2385c
Auburngrads <Auburngrads@live.com>
2016-02-14 16:06:31 -0500

    Update envir for teachingApp

--- R/teachingApp.R
+++ R/teachingApp.R
@@ -6,7 +6,7 @@ teachingApp <- function(name, link = FALSE) {
   gitPath <- "C:/Users/Jason/OneDrive/Work-Stuff/Computer Systems/GitHub/teaching-apps"
   gitURL  <- 'https://github.com/Auburngrads/teaching-apps/blob/master'
 
-  if(!link) dump(name, file = paste(c(gitPath, gitFile), collapse = '/'))
+  if(!link) dump(name, file = paste(c(gitPath, gitFile), collapse = '/'), envir = .GlobalEnv)
 
   if( link) paste(c(gitURL, gitFile), collapse = '/')
 
