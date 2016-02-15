c35008b616dab4422023bab6098f9837a3131977
Auburngrads <Auburngrads@live.com>
2016-02-15 16:54:05 -0500

    Added css option to teachingApp

--- R/teachingApp.R
+++ R/teachingApp.R
@@ -1,4 +1,4 @@
-teachingApp <- function(name, link = FALSE,...) {
+teachingApp <- function(name, link = FALSE, envir = NULL, css = TRUE,...) {
 
   if(!is.character(name)) stop('name must be a character string')
 
@@ -6,8 +6,16 @@ teachingApp <- function(name, link = FALSE,...) {
   gitPath <- "C:/Users/Jason/OneDrive/Work-Stuff/Computer Systems/GitHub/teaching-apps"
   gitURL  <- 'https://github.com/Auburngrads/teaching-apps/blob/master'
 
-  if(!link) dump(name, file = paste(c(gitPath, gitFile), collapse = '/'),... )
+  if(!link) dump(name, file = paste(c(gitPath, gitFile), collapse = '/'),envir = envir,... )
 
-  if( link) paste(c(gitURL, gitFile), collapse = '/')
+  if( link) {
+    if(css) {
 
+paste(c("<div style='font-size: 24; margin: 0 0 0 30px; padding: 0; font-weight: normal;'>View the code for this app [here](",
+                    paste(c(gitURL, gitFile), collapse = '/'),')</div>'), collapse = '')
+    } else {
+      paste(c(gitURL, gitFile), collapse = '/')
+
+}
+}
 }
