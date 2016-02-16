943cb3443f5ee4b9e1c7ae6fa2413b1f45f665d6
Auburngrads <Auburngrads@live.com>
2016-02-15 22:27:10 -0500

    Added 'cat' to text for teachingApp

--- R/teachingApp.R
+++ R/teachingApp.R
@@ -1,4 +1,4 @@
-teachingApp <- function(name, link = FALSE, envir = NULL, css = TRUE,...) {
+teachingApp <- function(name, link = TRUE, envir = NULL, css = TRUE,...) {
 
   if(!is.character(name)) stop('name must be a character string')
 
@@ -6,16 +6,21 @@ teachingApp <- function(name, link = FALSE, envir = NULL, css = TRUE,...) {
   gitPath <- "C:/Users/Jason/OneDrive/Work-Stuff/Computer Systems/GitHub/teaching-apps"
   gitURL  <- 'https://github.com/Auburngrads/teaching-apps/blob/master'
 
-  if(!link) dump(name, file = paste(c(gitPath, gitFile), collapse = '/'),envir = envir,... )
+  if(link) { dump(name, file = paste(c(gitPath, gitFile), collapse = '/'),envir = envir,... )
 
-  if( link) {
-    if(css) {
+  if(css) {
 
-paste(c("<div style='font-size: 22pt; margin: 0 0 20px 30px; font-weight: normal; border-top: 2px solid #95a5a6; border-bottom: 2px solid #95a5a6;'>View the [code](",
-                    paste(c(gitURL, gitFile), collapse = '/'),') for this app</div>'), collapse = '')
-    } else {
-      paste(c(gitURL, gitFile), collapse = '/')
+    cat(paste(c("<div style='font-size: 22pt; margin: 0 0 20px 30px; font-weight: normal; border-top: 2px solid #95a5a6; border-bottom: 2px solid #95a5a6;'>View the [code](",
+            paste(c(gitURL, gitFile), collapse = '/'),') for this app</div>'), collapse = ''))
+  } else {
+    paste(c(gitURL, gitFile), collapse = '/')
 
-}
+  }
+  }
+
+  if(!link) {
+
+    dump(name, file = paste(c(gitPath, gitFile), collapse = '/'),envir = envir,... )
+
 }
 }
