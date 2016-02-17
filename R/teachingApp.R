7dcc316eb784dec53d49b2c6f87d278b247fa672
Auburngrads <Auburngrads@live.com>
2016-02-16 20:31:28 -0500

    Added publish argument to teachingApp

--- R/teachingApp.R
+++ R/teachingApp.R
@@ -1,4 +1,4 @@
-teachingApp <- function(name, link = TRUE, css = TRUE,...) {
+teachingApp <- function(name, link = TRUE, css = TRUE, publish = FALSE,...) {
 
   if(!is.character(name)) stop('name must be a character string')
 
@@ -6,21 +6,25 @@ teachingApp <- function(name, link = TRUE, css = TRUE,...) {
   gitPath <- "C:/Users/Jason/OneDrive/Work-Stuff/Computer Systems/GitHub/teaching-apps"
   gitURL  <- 'https://github.com/Auburngrads/teaching-apps/blob/master'
 
-  if(link) { dump(name, file = paste(c(gitPath, gitFile), collapse = '/'),... )
+if(link & css & !publish) {
 
-  if(css) {
+   dump(name, file = paste(c(gitPath, gitFile), collapse = '/'),... )
 
-    cat(paste(c("<div style='font-size: 22pt; margin: 0 0 20px 30px; font-weight: normal;'>View the [code](",
+   cat(paste(c("<div style='font-size: 22pt; margin: 0 0 20px 30px; font-weight: normal;'>Get the [code](",
             paste(c(gitURL, gitFile), collapse = '/'),') for this app</div>'), collapse = ''))
-  } else {
-    paste(c(gitURL, gitFile), collapse = '/')
+
+}
+
+if(publish & css) {
+
+  cat(paste(c("<div style='font-size: 22pt; margin: 0 0 20px 30px; font-weight: normal;'>Get the [code](",
+              paste(c(gitURL, gitFile), collapse = '/'),') for this app</div>'), collapse = ''))
+
+}
+
+if(!css) {
+
+   paste(c(gitURL, gitFile), collapse = '/')
 
   }
   }
-
-  if(!link) {
-
-    dump(name, file = paste(c(gitPath, gitFile), collapse = '/'),envir = envir,... )
-
-}
-}
