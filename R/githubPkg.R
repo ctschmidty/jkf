37ce4742ef02cc4f150a9a83d0ca759a245859b9
Auburngrads <auburngrads@live.com>
2016-03-08 10:30:34 -0500

    Added `githubPkg`

---
+++ R/githubPkg.R
@@ -0,0 +1,19 @@
+githubPkg <- function(package = NULL, unzip = FALSE,...) {
+
+if(unzip) {
+
+  downloads <- paste(c('C:','Users',.cpu.name,'Downloads', paste(c(package,'-master.zip'),collapse = '')), collapse = '/')
+
+  current.directory <- getwd()
+
+  setwd(.github.directory )
+
+  unzip(downloads)
+
+  devtools::install(paste(c(getwd(),'/',package,'-master'), collapse = ''))
+
+  setwd(current.directory)
+
+} else { devtools::install(paste(c(.github.directory,'/',package,'-master'), collapse = ''))
+}
+}
