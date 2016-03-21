a8e86cbf73f13611c7012b31d1a6e7b273600df2
Auburngrads <Auburngrads@live.com>
2016-03-20 20:17:38 -0400

    Updated path for teaching apps

--- R/teachingApp.R
+++ R/teachingApp.R
@@ -3,15 +3,15 @@ teachingApp <- function(name, link = TRUE, css = TRUE, publish = FALSE,...) {
   if(!is.character(name)) stop('name must be a character string')
 
   gitFile <- paste(c(name,'.R'), collapse = '')
-  gitPath <- "C:/Users/Jason/OneDrive/Work-Stuff/Computer Systems/GitHub/teaching-apps"
-  gitURL  <- 'https://github.com/Auburngrads/teaching-apps/blob/master'
+  gitPath <- "C:/Users/Jason/OneDrive/Work-Stuff/Computer Systems/GitHub/teachingApps"
+  gitURL  <- 'https://github.com/Auburngrads/teachingApps/blob/master'
 
 if(link & css & !publish) {
 
    dump(name, file = paste(c(gitPath, gitFile), collapse = '/'),... )
 
-   cat(paste(c("<div style='font-size: 22pt; margin: 0 0 20px 30px; font-weight: normal;'>Get the [code](",
-            paste(c(gitURL, gitFile), collapse = '/'),') for this app</div>'), collapse = ''))
+   cat(paste(c("<div style='font-size: 21pt; margin: 0 0 20px 30px; font-weight: normal;'>Get the [code](",
+            paste(c(gitURL, gitFile), collapse = '/'),') for this app on GitHub</div>'), collapse = ''))
 
 }
 
