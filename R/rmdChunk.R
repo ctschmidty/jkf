f8d92721d447577cf6d124dfcbb9d0d1c7074a15
Auburngrads <Auburngrads@live.com>
2016-04-20 22:55:28 -0400

    rmdChunk

---
+++ R/rmdChunk.R
@@ -0,0 +1,30 @@
+rmdChunk <-
+  function(type = 'first')  {
+
+chunk <- switch(type,
+
+                'first' = {
+"
+```{r, echo=FALSE,message=FALSE, warning=FALSE}
+assign('dynamic', TRUE,envir = sys.frame(),immediate = TRUE)
+assign('publish', TRUE,envir = sys.frame(),immediate = TRUE)
+
+library(SMRD)
+library(xtable)
+library(jkf)
+
+knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = FALSE, results = 'asis')
+```"},
+
+                'last'  = {
+"
+```{r, echo=FALSE,message=FALSE, warning=FALSE}
+HTML('<link rel='stylesheet' type='text/css' href='css/flat-slidy.css'>')
+HTML('<script src='js/audiojs/audiojs/audio.min.js'></script>')
+HTML('<script> audiojs.events.ready(function() {audiojs.createAll();}); </script>')
+HTML('<script src='js/jkf-scroll.js'></script>')
+```"})
+
+print(noquote(gsub('\n','',chunk)))
+
+}
