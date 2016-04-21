2cd5ef4e1adc7db21dd929ca165eb1f32be54bde
Auburngrads <Auburngrads@live.com>
2016-04-21 09:11:40 -0400

    rmdChunk (last time?)

--- R/rmdChunk.R
+++ R/rmdChunk.R
@@ -3,9 +3,8 @@ rmdChunk <-
 
 chunk <- switch(type,
 
-                'first' = {
-"
-```{r, echo=TRUE,message=FALSE, warning=FALSE}
+                'first' = {function() {
+
 assign('dynamic', TRUE,envir = sys.frame(),immediate = TRUE)
 assign('publish', TRUE,envir = sys.frame(),immediate = TRUE)
 
@@ -13,16 +12,13 @@ library(SMRD)
 library(xtable)
 library(jkf)
 
-knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = FALSE, results = 'asis')
-```"},
+knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = FALSE, results = 'asis')}},
 
-                'last'  = {
-"
-```{r, echo=TRUE,message=FALSE, warning=FALSE}\n
-HTML('<link rel='stylesheet' type='text/css' href='css/flat-slidy.css'>')\n
-HTML('<script src='js/audiojs/audiojs/audio.min.js'></script>')\n
-HTML('<script> audiojs.events.ready(function() {audiojs.createAll();}); </script>')\n
-HTML('<script src='js/jkf-scroll.js'></script>')\n
-```"})
+                'last'  = {function() {
+HTML("<link rel='stylesheet' type='text/css' href='css/flat-slidy.css'>")
+HTML("<script src='js/audiojs/audiojs/audio.min.js'></script>")
+HTML("<script> audiojs.events.ready(function() {audiojs.createAll();}); </script>")
+HTML("<script src='js/jkf-scroll.js'></script>")
+}})
 
 }
