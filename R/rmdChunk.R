26e49f4f34543cd0c71321570071b605db0d44de
Auburngrads <Auburngrads@live.com>
2016-04-21 10:07:40 -0400

    Fingers crossed

--- R/rmdChunk.R
+++ R/rmdChunk.R
@@ -15,12 +15,13 @@ library(jkf)
 knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = FALSE, results = 'asis')}},
 
                 'last'  = {function() {
-HTML("<link rel='stylesheet' type='text/css' href='css/flat-slidy.css'>")
-HTML("<script src='js/audiojs/audiojs/audio.min.js'></script>")
-HTML("<script> audiojs.events.ready(function() {audiojs.createAll();}); </script>")
-HTML("<script src='js/jkf-scroll.js'></script>")
+list()
+HTML("<link rel='stylesheet' type='text/css' href='css/flat-slidy.css'>
+      <script src='js/audiojs/audiojs/audio.min.js'></script>
+      <script> audiojs.events.ready(function() {audiojs.createAll();}); </script>
+      <script src='js/jkf-scroll.js'></script>")
 }})
 
-chunk
+chunk()
 
 }
