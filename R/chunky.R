3bcb5c7fe0ce32d7d966f36245028ece2a3719f8
Auburngrads <Auburngrads@live.com>
2016-04-22 12:36:22 -0400

    fix chunky

--- R/chunky.R
+++ R/chunky.R
@@ -71,8 +71,8 @@ shinyApp(options = list(height = "600px"),
 teachingApp('ace_name', envir = environment(), publish = publish)
 }})
 
-if(tolower(type) == 'first'    | tolower(type) == 'last'  )  chunk()
-if(tolower(type) == 'shinyace' | tolower(type) == 'navbar')  chunk
+if(tolower(type)%in%c('first','last','vignette'))  chunk()
+if(tolower(type)%in%c('shinyace','navbar'))        chunk
 
 }
 
