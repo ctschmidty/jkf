b73940a14f69affedc33a5ee93b73a63ef50acfb
Auburngrads <Auburngrads@live.com>
2016-04-22 14:34:51 -0400

    remove \n from chunky type = 'last'

--- R/chunky.R
+++ R/chunky.R
@@ -16,8 +16,7 @@ knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = FALSE, results = 
 
                 'last'  = {function() {
 
-shiny::HTML(
-"<link rel='stylesheet' type='text/css' href='css/flat-slidy.css'>
+shiny::HTML("<link rel='stylesheet' type='text/css' href='css/flat-slidy.css'>
 <script src='js/audiojs/audiojs/audio.min.js'></script>
 <script> audiojs.events.ready(function() {audiojs.createAll();}); </script>
 <script src='js/jkf-scroll.js'></script>")}},
