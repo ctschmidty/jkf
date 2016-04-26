85dcd68d9cf477e184d8199097f4041436a1c83e
Auburngrads <Auburngrads@live.com>
2016-04-26 14:50:39 -0400

    added jkf_par to zzz

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,18 @@
 
   .github.directory <- 'J:/github-packages'
 }
+
+jkf.par <- function(...) {
+
+  par(cex.lab=1.05,
+      cex.axis=1.05,
+      mgp=c(2.25,.7,0),
+      tcl=-.3,
+      font.lab=2,
+      font=2,
+      font.axis=2,
+      las=1,
+      tck=0.015,
+      family='serif',...)
+
+}
