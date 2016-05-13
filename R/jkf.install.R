529bcf861b0ac3d44c6a847db196f26cf0cdc29e
Auburngrads <Auburngrads@live.com>
2016-05-13 10:55:39 -0400

    update jkf.install

---
+++ R/jkf.install.R
@@ -0,0 +1,9 @@
+jkf.install <- function(package = 'jkf',...) {
+
+  if(!package%in%c('jkf','teachingApps','SMRD')) stop('Uh...this is not one of your packages')
+
+  pkg <- paste(c('Auburngrads',package), collapse = '/')
+
+  devtools::install_github(pkg,auth_token = Sys.getenv('git.token'),...)
+
+}
