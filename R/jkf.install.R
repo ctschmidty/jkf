59fa5abaa1338bdb8cc57fb6228dc34874998844
Auburngrads <Auburngrads@live.com>
2016-05-13 11:04:26 -0400

    little fixes

--- R/jkf.install.R
+++ R/jkf.install.R
@@ -1,6 +1,6 @@
 jkf.install <- function(package = 'jkf',...) {
 
-  if(!package%in%c('jkf','teachingApps','SMRD')) stop('Uh...this is not one of your packages')
+  #if(!package%in%c('jkf','teachingApps','SMRD')) stop('Uh...this is not one of your packages')
 
   pkg <- paste(c('Auburngrads',package), collapse = '/')
 
